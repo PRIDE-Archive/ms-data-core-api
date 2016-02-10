7d9333fe96eff2e13160e162cb4ae1bd819087f8
Tobias-Ternent <tobias@ebi.ac.uk>
2016-02-10 14:32:14 +0000

    Updated genome absolute genome coordinates in the bed format (now working properly).

--- src/main/java/uk/ac/ebi/pride/utilities/data/exporters/MzTabBedConverter.java
+++ src/main/java/uk/ac/ebi/pride/utilities/data/exporters/MzTabBedConverter.java
@@ -96,7 +96,6 @@ public class MzTabBedConverter {
                                     break;
                             }
                         }
-                        int totalBlocksSize = 0;
                         List<String> blockStartsStrings = new ArrayList<>();
                         List<String> blockSizesStrings  = new ArrayList<>();
                         Collections.sort(positions);
@@ -141,14 +140,13 @@ public class MzTabBedConverter {
                                 blockStartsIntegers.add(new Integer(startIntegers.get(i) - Integer.parseInt(chromstart)));
                             }
 
-                            blockStartsStrings = new ArrayList<String>(blockStartsIntegers.size());
+                            blockStartsStrings = new ArrayList<>(blockStartsIntegers.size());
                             for (Integer myInt : blockStartsIntegers) {
                                 blockStartsStrings.add(String.valueOf(myInt));
                             }
-                            blockSizesStrings = new ArrayList<String>(blockSizesIntegers.size());
+                            blockSizesStrings = new ArrayList<>(blockSizesIntegers.size());
                             for (Integer myInt : blockSizesIntegers) {
                                 blockSizesStrings.add(String.valueOf(myInt));
-                                totalBlocksSize += myInt.intValue();
                             }
                             blockStarts = StringUtils.join(blockStartsStrings, ",");
                             blockSizes = StringUtils.join(blockSizesStrings, ",");
@@ -163,111 +161,112 @@ public class MzTabBedConverter {
                                 }
                             }
                         }
-                        ArrayList<String> peptideModifications = new ArrayList<>();
-                        ArrayList<String> genomeModifications = new ArrayList<>();
-                        for (Modification modification : peptideEvidence.getPeptideSequence().getModifications()) {
-                            int peptideLocation = modification.getLocation();
-                            int genomeLocation;
-                            final int TOTAL = peptideEvidence.getPeptideSequence().getSequence().length();
-                            double i = modification.getLocation() / TOTAL;
-                            double j = i * totalBlocksSize;
-                            int blockIndex = 0;
-                            int currentBlockTotal = 0;
-                            genomeLocation = (int) Math.round(j);
-                            for (String blockSize : blockSizesStrings) {
-                                int block = Integer.valueOf(blockSize);
-                                currentBlockTotal += block;
-                                if (j <= currentBlockTotal) {
-                                    genomeLocation = Integer.valueOf(blockStartsStrings.get(blockIndex)) + genomeLocation;
-                                    break;
+                        if (blockStartsStrings!=null && blockStartsStrings.size()>0) {
+                            ArrayList<String> peptideModifications = new ArrayList<>();
+                            ArrayList<String> genomeModifications = new ArrayList<>();
+                            for (Modification modification : peptideEvidence.getPeptideSequence().getModifications()) {
+                                final int RELATIVE_PEP_LOCATION = modification.getLocation();
+                                int relativeGenomeLocation = RELATIVE_PEP_LOCATION * 3;
+                                int absoluteGenomeLocation =  Integer.valueOf(chromstart) + Integer.valueOf(blockStartsStrings.get(0)) + relativeGenomeLocation;
+                                int blockIndex = 0;
+                                int currentBlockTotal = 0;
+                                if (blockStartsStrings.size()>1) {
+                                    for (String blockSizeStr : blockSizesStrings) {
+                                        int blockSize = Integer.valueOf(blockSizeStr);
+                                        currentBlockTotal += blockSize;
+                                        if (relativeGenomeLocation <= currentBlockTotal) {
+                                            absoluteGenomeLocation = Integer.valueOf(chromstart) + Integer.valueOf(blockStartsStrings.get(blockIndex)) + relativeGenomeLocation;
+                                            break;
+                                        }
+                                        relativeGenomeLocation -= blockSize;
+                                        blockIndex++;
+                                    }
                                 }
-                                genomeLocation -= block;
-                                blockIndex++;
+                                for (CvParam cvParam : modification.getCvParams()) {
+                                    peptideModifications.add(RELATIVE_PEP_LOCATION + "-" + cvParam.getAccession());
+                                    genomeModifications.add(absoluteGenomeLocation + "-" + cvParam.getAccession());
+                                }
                             }
-                            for (CvParam cvParam : modification.getCvParams()) {
-                                peptideModifications.add(peptideLocation + "-" + cvParam.getAccession());
-                                genomeModifications.add(genomeLocation + "-" + cvParam.getAccession());
+                            if (peptideModifications.size() > 0) {
+                                pepMods = StringUtils.join(peptideModifications, ", ");
+                                genMods = StringUtils.join(genomeModifications, ", ");
                             }
-                        }
-                        if (peptideModifications.size() > 0) {
-                            pepMods = StringUtils.join(peptideModifications, ", ");
-                            genMods = StringUtils.join(genomeModifications, ", ");
-                        }
 
-                        if (!chrom.equalsIgnoreCase("null")) {
-                            stringBuilder.append(chrom); // chrom
-                            stringBuilder.append('\t');
-                            stringBuilder.append(chromstart); // chromstart
-                            stringBuilder.append('\t');
-                            stringBuilder.append(chromend); // chromend
-                            stringBuilder.append('\t');
-                            String name =  protein.getDbSequence().getName();
-                            if (!projectAccession.isEmpty()) {
-                                name = name + "_" + projectAccession;
-                            }
-                            if (!assayAccession.isEmpty()) {
-                                name = name + "_" + assayAccession;
-                            }
-                            name = name + "_" + ++lineNumber;
-                            stringBuilder.append(name); // name
-                            stringBuilder.append('\t');
-                            stringBuilder.append(1000); // score (1000)
-                            stringBuilder.append('\t');
-                            stringBuilder.append(strand); // strand
-                            stringBuilder.append('\t');
-                            stringBuilder.append(chromstart); // thickStart
-                            stringBuilder.append('\t');
-                            stringBuilder.append(chromend); // thickEnd
-                            stringBuilder.append('\t');
-                            stringBuilder.append("0"); // reserved - (0 only)
-                            stringBuilder.append('\t');
-                            stringBuilder.append(blockCount); // blockCount
-                            stringBuilder.append('\t');
-                            stringBuilder.append(blockSizes); // blockSizes
-                            stringBuilder.append('\t');
-                            stringBuilder.append(blockStarts); // chromStarts (actual name, but refers to blocks)
-                            stringBuilder.append('\t') ;
-                            stringBuilder.append(protein.getDbSequence().getName()); // proteinAccession
-                            stringBuilder.append('\t') ;
-                            stringBuilder.append(peptideEvidence.getPeptideSequence().getSequence());  // peptideSequence
-                            stringBuilder.append('\t');
-                            stringBuilder.append(psmScore); // psmScore
-                            stringBuilder.append('\t');
-                            stringBuilder.append(fdrScore); // fdrScore
-                            stringBuilder.append('\t');
-                            stringBuilder.append(pepMods); // peptide location modifications
-                            stringBuilder.append('\t');
-                            stringBuilder.append(genMods); // genome location modifications
-                            stringBuilder.append('\t');
-                            stringBuilder.append(peptide.getPrecursorCharge()); // charge
-                            stringBuilder.append('\t');
-                            stringBuilder.append(peptide.getSpectrumIdentification().getExperimentalMassToCharge()); // expMassToCharge
-                            stringBuilder.append('\t');
-                            stringBuilder.append(peptide.getSpectrumIdentification().getCalculatedMassToCharge()); // calcMassToCharge
-                            stringBuilder.append('\t');
-                            String datasetID = "";
-                            if (!projectAccession.isEmpty()) {
-                                datasetID = projectAccession;
-                                if (!assayAccession.isEmpty()) {
-                                    datasetID = datasetID + "_" + assayAccession;
+                            if (!chrom.equalsIgnoreCase("null")) {
+                                stringBuilder.append(chrom); // chrom
+                                stringBuilder.append('\t');
+                                stringBuilder.append(chromstart); // chromstart
+                                stringBuilder.append('\t');
+                                stringBuilder.append(chromend); // chromend
+                                stringBuilder.append('\t');
+                                String name =  protein.getDbSequence().getName();
+                                if (!projectAccession.isEmpty()) {
+                                    name = name + "_" + projectAccession;
                                 }
-                            } else {
-                                datasetID = mzTabController.getReader().getMetadata().getMZTabID();
+                                if (!assayAccession.isEmpty()) {
+                                    name = name + "_" + assayAccession;
+                                }
+                                name = name + "_" + ++lineNumber;
+                                stringBuilder.append(name); // name
+                                stringBuilder.append('\t');
+                                stringBuilder.append(1000); // score (1000)
+                                stringBuilder.append('\t');
+                                stringBuilder.append(strand); // strand
+                                stringBuilder.append('\t');
+                                stringBuilder.append(chromstart); // thickStart
+                                stringBuilder.append('\t');
+                                stringBuilder.append(chromend); // thickEnd
+                                stringBuilder.append('\t');
+                                stringBuilder.append("0"); // reserved - (0 only)
+                                stringBuilder.append('\t');
+                                stringBuilder.append(blockCount); // blockCount
+                                stringBuilder.append('\t');
+                                stringBuilder.append(blockSizes); // blockSizes
+                                stringBuilder.append('\t');
+                                stringBuilder.append(blockStarts); // chromStarts (actual name, but refers to blocks)
+                                stringBuilder.append('\t') ;
+                                stringBuilder.append(protein.getDbSequence().getName()); // proteinAccession
+                                stringBuilder.append('\t') ;
+                                stringBuilder.append(peptideEvidence.getPeptideSequence().getSequence());  // peptideSequence
+                                stringBuilder.append('\t');
+                                stringBuilder.append(psmScore); // psmScore
+                                stringBuilder.append('\t');
+                                stringBuilder.append(fdrScore); // fdrScore
+                                stringBuilder.append('\t');
+                                stringBuilder.append(pepMods); // peptide location modifications
+                                stringBuilder.append('\t');
+                                stringBuilder.append(genMods); // genome location modifications
+                                stringBuilder.append('\t');
+                                stringBuilder.append(peptide.getPrecursorCharge()); // charge
+                                stringBuilder.append('\t');
+                                stringBuilder.append(peptide.getSpectrumIdentification().getExperimentalMassToCharge()); // expMassToCharge
+                                stringBuilder.append('\t');
+                                stringBuilder.append(peptide.getSpectrumIdentification().getCalculatedMassToCharge()); // calcMassToCharge
+                                stringBuilder.append('\t');
+                                String datasetID = "";
+                                if (!projectAccession.isEmpty()) {
+                                    datasetID = projectAccession;
+                                    if (!assayAccession.isEmpty()) {
+                                        datasetID = datasetID + "_" + assayAccession;
+                                    }
+                                } else {
+                                    datasetID = mzTabController.getReader().getMetadata().getMZTabID();
+                                }
+                                stringBuilder.append(datasetID); // datasetID
+                                stringBuilder.append('\t');
+                                String projectUri ;
+                                if (!projectAccession.isEmpty()) {
+                                    projectUri = "http://www.ebi.ac.uk/pride/archive/projects/" + projectAccession;
+                                }  else {
+                                    projectUri = "null";
+                                }
+                                stringBuilder.append(projectUri); // projectURI
+                                stringBuilder.append('\n');
+                                bf.write(stringBuilder.toString());
+                                bf.flush();
                             }
-                            stringBuilder.append(datasetID); // datasetID
-                            stringBuilder.append('\t');
-                            String projectUri ;
-                            if (!projectAccession.isEmpty()) {
-                                projectUri = "http://www.ebi.ac.uk/pride/archive/projects/" + projectAccession;
-                            }  else {
-                                projectUri = "null";
-                            }
-                            stringBuilder.append(projectUri); // projectURI
-                            stringBuilder.append('\n');
-                            bf.write(stringBuilder.toString());
-                            bf.flush();
+                            stringBuilder.setLength(0);
                         }
-                        stringBuilder.setLength(0);
                     }
                 }
             }
