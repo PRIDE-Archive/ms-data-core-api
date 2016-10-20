c5120e968a5d0c3325cb8934c85570cffddd549f
Tobias Ternent <tobias@ebi.ac.uk>
2016-10-20 17:42:38 +0100

    Refactored validation-related methods, added new parent class for 'assay' files that share similar functionality.

--- src/main/java/uk/ac/ebi/pride/utilities/data/controller/impl/ControllerImpl/ReferencedIdentificationController.java
+++ src/main/java/uk/ac/ebi/pride/utilities/data/controller/impl/ControllerImpl/ReferencedIdentificationController.java
@@ -23,7 +23,7 @@ import java.util.*;
  * @author Yasset Perez-Riverol
  * @author Rui Wang
  */
-public abstract class ReferencedIdentificationController extends CachedDataAccessController {
+public abstract class ReferencedIdentificationController extends AssayFileController {
 
     // Logger property to trace the Errors
     private static final Logger logger = LoggerFactory.getLogger(ReferencedIdentificationController.class);
@@ -568,60 +568,5 @@ public abstract class ReferencedIdentificationController extends CachedDataAcces
         return super.getProteinAmbiguityGroupIds().size() > 0;
     }
 
-    /**
-     * This function check randomly if the spectra is well referenced for a couple of spectra
-     * for that it is using the the number of Spectra to be check.
-     * @param numberSpectra the number of spectra to be checked
-     * @return boolean if all the reference as fine
-     */
-    public boolean checkRandomSpectraByDeltaMassThreshold(int numberSpectra, Double deltaThreshold){
-        boolean result = true;
-        List<Comparable> listIds = new ArrayList<>(getProteinIds());
-        Random r = new Random();
-        int randomProtIdNumb;
-        int randomPepIdNumb;
-        if(hasSpectrum()) {
-            for (int i=0; i < numberSpectra && result; i++){
-                randomProtIdNumb = r.ints(0, listIds.size()).findFirst().getAsInt();
-                Comparable proteinId = listIds.get(randomProtIdNumb);
-                Protein protein = getProteinById(proteinId);
-                randomPepIdNumb = r.ints(0, protein.getPeptides().size()).findFirst().getAsInt();
-                Peptide peptide = protein.getPeptides().get(randomPepIdNumb);
-                if (peptide==null) {
-                  logger.error("Random peptide is null! Index:" + randomPepIdNumb);
-                    result = false;
-                } else {
-                    Spectrum spectrum = getSpectrumById(peptide.getSpectrumIdentification().getId());
-                    Integer charge = getPeptidePrecursorCharge(proteinId, peptide.getId());
-                    double mz = getPeptidePrecursorMz(proteinId, peptide.getId());
-                    List<Double> ptmMasses = new ArrayList<>();
-                    for (Modification mod : peptide.getModifications()) {
-                        List<Double> monoMasses = mod.getMonoisotopicMassDelta();
-                        if (monoMasses != null && !monoMasses.isEmpty())
-                            ptmMasses.add(monoMasses.get(0));
-                    }
-
-                    if ((charge == null || mz == -1)) {
-                        charge = getSpectrumPrecursorCharge(spectrum.getId());
-                        mz = getSpectrumPrecursorMz(spectrum.getId());
-                        if (charge != null && charge == 0) {
-                            charge = null;
-                        }
-                    }
-                    if (charge == null) {
-                        result = false;
-                    } else {
-                        Double deltaMass = MoleculeUtilities.calculateDeltaMz(peptide.getSequence(), mz, charge, ptmMasses);
-                        if (deltaMass == null || Math.abs(deltaMass) > deltaThreshold) {
-                            result = false;
-                        }
-                    }
-                }
-
-            }
-        } else {
-            result = false;
-        }
-        return result;
-    }
+    abstract public List<SpectraData> getSpectraDataFiles();
 }
