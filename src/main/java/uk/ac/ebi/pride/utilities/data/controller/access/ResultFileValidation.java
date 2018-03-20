17fb1b64670e40967a1aa89367a71eb456f75e48
Tobias Ternent <tobias@ebi.ac.uk>
2018-03-20 09:14:14 +0000

    Added JavaDoc.

--- src/main/java/uk/ac/ebi/pride/utilities/data/controller/access/ResultFileValidation.java
+++ src/main/java/uk/ac/ebi/pride/utilities/data/controller/access/ResultFileValidation.java
@@ -1,8 +1,8 @@
 package uk.ac.ebi.pride.utilities.data.controller.access;
 
 /**
- * This interface define a set of functions that should be implemented for any of the ResultFiles to Validate the file at
- * ms-data-core-api level for the Pipeline.
+ * This interface defines a set of functions that should be implemented for any of the ResultFiles to Validate the file at the
+ * ms-data-core-api level for the submission pipeline.
  *
  * @author ypriverol
  * @version $Id$
@@ -11,13 +11,13 @@ public interface ResultFileValidation {
 
     /**
      * This is needed in the Validation file to know how many spectra the current ResultFile Referenced
-     * @return
+     * @return the number of spectra
      */
     Integer getNumberOfSpectra();
 
     /**
      * Perform randomly spectra validation within the result file.
-     * @param numberOfRandomChecks
+     * @param numberOfRandomChecks the number of random checks
      */
     void doSpectraValidation(final int numberOfRandomChecks);
 }
