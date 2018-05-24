25c1a14e14ce7be4101528b946fb7508e3582780
Suresh Hewapathirana <hewapathirana@ebi.ac.uk>
2018-05-24 18:08:19 +0100

    change Test case

--- src/test/java/uk/ac/ebi/pride/utilities/data/io/file/FastMzIdentMLUnmarshallerAdaptorTest.java
+++ src/test/java/uk/ac/ebi/pride/utilities/data/io/file/FastMzIdentMLUnmarshallerAdaptorTest.java
@@ -1,17 +1,17 @@
 package uk.ac.ebi.pride.utilities.data.io.file;
 
-import com.jamonapi.Monitor;
-import com.jamonapi.MonitorFactory;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.File;
 import java.net.URL;
 
 /**
  * @author Suresh Hewapathirana
+ */
+
+/**
+ * This class only do few testing to verify that mzIdentML unmarshalling works fine.
+ * Rest of the other methods are tested by FastMzIdentMLControllerTest class.
  */
 public class FastMzIdentMLUnmarshallerAdaptorTest {
 
@@ -29,28 +29,14 @@ public class FastMzIdentMLUnmarshallerAdaptorTest {
       this.fastValidateMzIdentMLUnmarshaller = new FastMzIdentMLUnmarshallerAdaptor(inputFile);
     }
 
-//    @Test
-//    public void doUnmarshell() {
-//            Monitor monitor= MonitorFactory.start("uk.ac.ebi.pride.utilities.data.io.file.doUnmarshell");
-//            this.fastValidateMzIdentMLUnmarshaller = new FastMzIdentMLUnmarshallerAdaptor(new File("<File full path here>"));
-//            monitor.stop();
-//            System.out.println("Performance INFO: --------------  " + monitor);
-//    }
-
+  /**
+   * Test few functions to see unmarshalling works fine
+   */
   @Test
-  public void isSpectraDataReferencedByTitle() {
-    Assert.assertFalse("Spectra of this MzIdentML is not referred by title", fastValidateMzIdentMLUnmarshaller.isSpectraDataReferencedByTitle(fastValidateMzIdentMLUnmarshaller.getSpectraData().get(0)));
-  }
-
-  @Test
-  public void getVersion() {
+  public void testFastMzIdentMLUnmarshalling() {
     Assert.assertEquals("MzIdentML Version should be 1.1.0","1.1.0", fastValidateMzIdentMLUnmarshaller.getVersion());
-  }
-
-
-  @Test
-  public void getCvList() {
     Assert.assertEquals("small.mzid file first CV in the CvList should be PSI-MS","PSI-MS", fastValidateMzIdentMLUnmarshaller.getCvList().get(0).getId());
+    Assert.assertFalse("Spectra of this MzIdentML is not referred by title", fastValidateMzIdentMLUnmarshaller.isSpectraDataReferencedByTitle(fastValidateMzIdentMLUnmarshaller.getSpectraData().get(0)));
   }
 
   @After
